5f9af6a66fa67e52e036be6e49d5bca03b145d0f
robin hardjowijono <robinhardjowijono@hotmail.com>
2021-10-15 12:49:34 +0200

    Klaaaaaaaaaaaaaaaaaaaar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let segments = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
 ];
 
 for (let i = 0; i < segments.length; i++) {
@@ -12,7 +12,8 @@ for (let i = 0; i < segments.length; i++) {
 }
 
 function rotateClick() {
-  rot = Math.round(Math.random() * 360);
+  rot = Math.round(Math.random() * 1440);
   document.getElementById("container").style.transform =
     "rotate(" + rot + "deg)";
 }
+
