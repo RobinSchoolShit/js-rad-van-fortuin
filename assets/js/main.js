b9d00aa1cd7912d16bcca95d5d8465128c5e7a78
robin hardjowijono <robinhardjowijono@hotmail.com>
2021-10-15 11:08:14 +0200

    Rotate onclick werkt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
-let segments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let segments = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 
 for (let i = 0; i < segments.length; i++) {
   // console.log(segments[i]);
   var segment = document.createElement("div");
   segment.innerHTML = segments[i];
-  document.body.appendChild(segment);
+  document.getElementById("container").appendChild(segment);
   segment.classList.add("segmentClass");
+  segment.style.transform = "rotate(" + (360 / segments.length) * i + "deg)";
+}
+
+function rotateClick() {
+  rot = Math.round(Math.random() * 360);
+  document.getElementById("container").style.transform =
+    "rotate(" + rot + "deg)";
 }
